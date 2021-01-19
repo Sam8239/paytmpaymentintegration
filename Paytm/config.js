236c9bc9bbf63293292f1d230d46ace359a65775
Shubham <coolshubham1999@gmail.com>
2021-01-20 01:38:23 +0530

    First Commit

---
+++ Paytm/config.js
@@ -0,0 +1,6 @@
+var PaytmConfig = {
+  mid: "PVHUEM22505413450964",
+  key: "%Bju4qprwddG1BX!",
+  website: "WEBSTAGING",
+};
+module.exports.PaytmConfig = PaytmConfig;
---
+++ Readme.md.txt
@@ -0,0 +1,4 @@
+To run this file:- 
+1. npm install
+2. npm start
+3. Go to http://localhost:3000 
